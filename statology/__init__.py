4316e7f8a98e7c7de005565573499c4cc081abd0
Debmalya Pramanik <debmalyapramanik.005@gmail.com>
2024-12-11 17:01:27 +0530

    🎉 initialize module statology

---
+++ statology/__init__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+"""
+A Collection of Statistical Function(s)
+
+Statistics is the backbone of Data Science and Analytics and the module exposes
+a collection of functions which acts as a wrapper between external libraries
+like :mod:`numpy`, :mod:`scipy.stats` etc.
+
+@author: Debmalya Pramanik
+@copywright: 2024; SharkUtilities, ZenithClown
+"""
+
+# ? package follows https://peps.python.org/pep-0440/
+# ? https://python-semver.readthedocs.io/en/latest/advanced/convert-pypi-to-semver.html
+__version__ = "v0.0.1.dev0"
+
+# init-time options registrations
